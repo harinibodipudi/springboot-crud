bfafb8d1c2ae740cfdd57dfc80ed54c78b0581d6
hari <harini.bodipudi@thoughtworks.com>
2022-01-27 20:55:19 +0530

    First Commit

---
+++ src/test/java/com/crud/restapicrud/RestapiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.restapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
